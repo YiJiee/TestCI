51f751be1906e7f7c46056a59b50c8a94ea4a704
yijie <yijieelim@gmail.com>
2021-02-16 01:03:59 +0800

    try

--- Team11/Code11/src/unit_testing/src/TestAST.cpp
+++ Team11/Code11/src/unit_testing/src/TestAST.cpp
@@ -2,18 +2,22 @@
 #include "TNode.h"
 
 #include "catch.hpp"
+#include <fstream>
+#include <iostream>
 using namespace std;
-void require(bool b) {
-    REQUIRE(b);
-}
 
 TEST_CASE("1st Test") {
 
     TNode T;
 	
+	myfile.open("../../../../tests/Sample_queries.txt");
+	while (getline(myfile, myText)) {
+		// Output the text from the file
+		cout << myText << endl;
+	}
+	myfile.close();
 	
-	
-    require(1 == 2);
+    require(1 == 1);
 }
 
 
